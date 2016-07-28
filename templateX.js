3a61103d5277a9d74a08853d1ec5fc8df6dceb50
Kamil Głowacki <glowacki.kam@gmail.com>
2016-07-28 23:00:53 +0200

    ifStatement function

--- templateX.js
+++ templateX.js
@@ -9,14 +9,67 @@ var templateX = {};
         
         tX = nspace.render = function (template, dataObj) {
             data = dataObj;
-            return _render(template);
+            return  _render(template);
         },
         getdata = function (opt) {
+            var tag = opt.substring; 
+            if (tag.charAt(0) === '#') return _ifStatement(opt, true);
+            if (tag.charAt(0) === '^') return _ifStatement(opt, false);
             return opt.strBegining + _renderTag(opt.substring) + opt.strEnding;
         };
     
         //Private utilities
-    
+        function _render(template) {
+            console.log(template);
+            var indexStart = template.indexOf('{{'),
+                indexEnd = template.indexOf('}}'),
+                opt,
+                substring,
+                toReplace = '';
+                
+            if (indexStart === -1 || indexEnd === -1) {
+                return template.replace(/\%\{/gi, '{');
+            }
+        
+            opt = {
+                strBegining: template.slice(0, indexStart),
+                strEnding: template.slice(indexEnd + 2),
+                substring: template.slice(indexStart + 2, indexEnd)
+            };
+        
+            return _render(getdata(opt));
+        };
+        function _ifStatement(opt, bol) {
+            var tag = opt.substring.slice(1),
+                endTag = '{{/' + tag + '}}',
+                indexEnd = opt.strEnding.indexOf(endTag),
+                ifContent, ifVal;    
+
+            
+            if (indexEnd === -1) { console.error("Missing ending tag for {{" + opt.substring + "}}!!");} 
+            
+            ifContent = opt.strEnding.slice(0, indexEnd);
+            opt.strEnding = opt.strEnding.slice(indexEnd + endTag.length);
+            ifVal = _renderTag(tag) + '';
+            
+            if (bol) {
+                if (ifVal) { 
+                    return opt.strBegining + ifContent + opt.strEnding;
+                } else { 
+                    return opt.strBegining + opt.strEnding;
+                }
+                
+            } else {
+                
+                if (!ifVal) {
+                    return opt.strBegining + ifContent + opt.strEnding;   
+                } else { 
+                    return opt.strBegining + opt.strEnding;
+                }
+                
+            }
+            
+        };
         function _renderTag(tagStr) {
             var dataObj = data,
                 ns = tagStr.split('.'),
@@ -41,27 +94,6 @@ var templateX = {};
                 };
             };
             return dataObj;
-        };
-    
-        function _render(template) {
-            
-            var indexStart = template.indexOf('{{'),
-                indexEnd = template.indexOf('}}'),
-                opt,
-                substring,
-                toReplace = '';
-                
-            if (indexStart === -1 || indexEnd === -1) {
-                return template.replace(/\%\{/gi, '{');
-            }
-        
-            opt = {
-                strBegining: template.slice(0, indexStart),
-                strEnding: template.slice(indexEnd + 2),
-                substring: template.slice(indexStart + 2, indexEnd)
-            };
-        
-            return _render(getdata(opt));
         };
     
         //set short alias for templateX;
